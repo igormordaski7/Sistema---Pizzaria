28addd688714a5b4f9def24327492d5b66642ed0
Nagel <130867271+Nagel-P@users.noreply.github.com>
2024-06-17 20:06:39 -0300

    Update Bebidas.java
    atualização da classe bebidas

--- Sistema Pizzaria/src/Bebidas.java
+++ Sistema Pizzaria/src/Bebidas.java
@@ -1,15 +1,13 @@
 public class Bebidas {
-    // Atributos da classe
     private String nome;
     private float preco;
 
-    // Construtor da classe
     public Bebidas(String nome, float preco) {
         this.nome = nome;
         this.preco = preco;
     }
 
-    // Métodos para obter os valores dos atributos
+    // Métodos getter
     public String getNome() {
         return nome;
     }
@@ -18,20 +16,12 @@ public class Bebidas {
         return preco;
     }
 
-    // Método para exibir as informações das bebidas
-    public void exibirInfo() {
-        System.out.println("Nome: " + nome + ", Preço: R$" + preco);
-    }
-
-    // Método principal para executar o código
-    public static void main(String[] args) {
-        // Criando os objetos da classe Bebidas
-        Bebidas bebida1 = new Bebidas("coca 2L", 10.00f);
-        Bebidas bebida2 = new Bebidas("guarana antartica 2L", 10.00f);
-        Bebidas bebida3 = new Bebidas("fanta 2L", 10.00f);
-        Bebidas bebida4 = new Bebidas("cini 2L", 8.00f);
-        Bebidas bebida5 = new Bebidas("guarana kuat 2L", 8.00f);
-        Bebidas bebida6 = new Bebidas("coca 600mL", 7.00f);
-        Bebidas bebida7 = new Bebidas("guarana antartica 600mL", 7.00f);
-    }
+    // Criação dos objetos base de bebidas
+    public static Bebidas b1 = new Bebidas("coca 2L", 10.00f);
+    public static Bebidas b2 = new Bebidas("guarana antartica 2L", 10.00f);
+    public static Bebidas b3 = new Bebidas("fanta 2L", 10.00f);
+    public static Bebidas b4 = new Bebidas("cini 2L", 8.00f);
+    public static Bebidas b5 = new Bebidas("guarana kuat 2L", 8.00f);
+    public static Bebidas b6 = new Bebidas("coca 600mL", 7.00f);
+    public static Bebidas b7 = new Bebidas("guarana antartica 600mL", 7.00f);
 }
