e0228e9c91ce4f24a5ff5476cc54d31ed72c34f2
felipewnaka <nakamurinha823@gmail.com>
2024-06-21 23:01:24 -0300

    Alteração das Bebidas

--- Sistema Pizzaria/src/Bebidas.java
+++ Sistema Pizzaria/src/Bebidas.java
@@ -17,11 +17,19 @@ public class Bebidas {
     }
 
     // Criação dos objetos base de bebidas
-    public static Bebidas b1 = new Bebidas("coca 2L", 10.00f);
-    public static Bebidas b2 = new Bebidas("guarana antartica 2L", 10.00f);
-    public static Bebidas b3 = new Bebidas("fanta 2L", 10.00f);
-    public static Bebidas b4 = new Bebidas("cini 2L", 8.00f);
-    public static Bebidas b5 = new Bebidas("guarana kuat 2L", 8.00f);
-    public static Bebidas b6 = new Bebidas("coca 600mL", 7.00f);
-    public static Bebidas b7 = new Bebidas("guarana antartica 600mL", 7.00f);
+    public static Bebidas b1 = new Bebidas("Coca 2L", 10.00f);
+    public static Bebidas b2 = new Bebidas("Guarana antartica 2L", 10.00f);
+    public static Bebidas b3 = new Bebidas("Fanta Laranja 2L", 10.00f);
+    public static Bebidas b4 = new Bebidas("Fanta Uva 2L", 10.00f);
+    public static Bebidas b5 = new Bebidas("Cini 2L", 8.00f);
+    public static Bebidas b6 = new Bebidas("Guarana Kuat 2L", 8.00f);
+    public static Bebidas b7 = new Bebidas("Sprite 2L", 7.50f);
+    public static Bebidas b8 = new Bebidas("Chá Matte Limão 1.5L", 8.00f);
+    public static Bebidas b9 = new Bebidas("Chá Matte Pêssego 1.5L", 8.00f);
+    public static Bebidas b10 = new Bebidas("Suco Prats de Laranja 900mL", 7.50f);
+    public static Bebidas b11 = new Bebidas("Suco Prats de Uva 900ml", 7.50f);
+    public static Bebidas b12 = new Bebidas("Coca 600mL", 7.00f);
+    public static Bebidas b13 = new Bebidas("Guarana Antartica 600mL", 7.00f);
+    public static Bebidas b14 = new Bebidas("Água com gás 500mL", 5.00f);
+    public static Bebidas b15 = new Bebidas("Água Mineral 500mL", 4.50f);
 }
